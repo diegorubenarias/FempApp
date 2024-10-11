832fb920750a5204d52ab414bc5c19ec2f0240cd
diego <diego.arias@gmail.com>
2024-10-11 17:15:02 -0300

    create project structure

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fempa',
+  appName: 'FempaApp',
+  webDir: 'dist'
+};
+
+export default config;
